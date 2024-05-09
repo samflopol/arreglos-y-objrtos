5a6be9ad962e1ed439ff2fd6b149eba0605f741b
samflopol <sflorez@uniempresarial.edu.co>
2024-05-08 20:20:08 -0500

    tablas

---
+++ js/tabla.js
@@ -0,0 +1,43 @@
+const AlumnosMatriculados=[{
+    nombre:"pedro",
+    apellido:"perez",
+    rut:"1-9",
+    cod_curso:"AE600",
+    descripcion:"algoritmos y estructura de datos",
+},
+{
+    nombre:"juan",
+    apellido:"jara",
+    rut:"2-7",
+    cod_curso:"BD253",
+    descripcion:"base de datos",
+},
+{
+    nombre:"juan",
+    apellido:"jara",
+    rut:"2-7",
+    cod_curso:"AE600",
+    descripcion:"algoritmos y estructura de datos",
+},
+{
+    nombre:"diego",
+    apellido:"diaz",
+    rut:"3-5",
+    cod_curso:"BD253",
+    descripcion:"base de datos",
+},
+{
+    nombre:"maria",
+    apellido:"martinez",
+    rut:"4-4",
+    cod_curso:"BD253",
+    descripcion:"base de datos",
+},
+]
+
+for (let i= 0; i < AlumnosMatriculados.length; i++){
+    console.log(AlumnosMatriculados[i]);
+}
+
+
+
